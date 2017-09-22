da3d8140d4d7e3a095e1fbfb5b2b0899f619a053
Young <young.mu@aliyun.com>
2017-09-22 13:53:26 +0800

    Remove cp-config.sh

--- template/cp-config.sh
+++
@@ -1,3 +0,0 @@
-cp ../configs/device/$1-device.json ./configs/device.json
-cp ../configs/connector/$1-connector.json ./configs/connector.json
-cp ../configs/sys_config/$1-sys.json ./configs/sys-config.json
