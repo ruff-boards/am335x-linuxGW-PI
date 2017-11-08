0d6bcd8afd910871630c5172c19f13ff59a7eb13
Young <young.mu@aliyun.com>
2017-11-08 09:30:51 +0800

    Update src/index.js

--- template/src/index.js
+++ template/src/index.js
@@ -12,27 +12,24 @@ $.ready(function (error) {
     var DI = require('data-integration');
     var DU = require('data-upload');
 
-    var deviceConfigFile = fs.readFileSync('configs/device.json');
-    var deviceConfigRson = RSON.rson(RSON.binify(JSON.parse(deviceConfigFile)));
+    var appConfigFile = fs.readFileSync('configs/app-config.json');
+    var appConfigRson = RSON.rson(RSON.binify(JSON.parse(appConfigFile)));
 
-    var connConfigFile = fs.readFileSync('configs/connector.json');
-    var connConfigRson = RSON.rson(RSON.binify(JSON.parse(connConfigFile)));
-
-    var dpe = new DPE(deviceConfigRson, { debug: false });
+    var dpe = new DPE(appConfigRson, { debug: false });
     dpe.start();
     dpe.on('message', function (message) {
         //console.log(JSON.stringify(message, false, 4));
         di.store(message);
     });
 
-    var di = new DI(deviceConfigRson, { debug: false });
+    var di = new DI(appConfigRson, { debug: false });
     di.start();
     di.on('message', function (message) {
         //console.log(JSON.stringify(message, false, 4));
         du.store(message);
     });
 
-    var du = new DU(deviceConfigRson, connConfigRson, { debug: true });
+    var du = new DU(appConfigRson, { debug: true });
     du.start();
 });
 
